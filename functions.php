6eddaaf0043b673f4c540698107dbf7d2a1011e6
Uttam Marandi <uttammarandi007@gmail.com>
2020-12-22 14:06:13 +0530

    single product page modified , related product no. changed in function.php

--- functions.php
+++ functions.php
@@ -17,3 +17,22 @@ if (!function_exists('loop_columns')) {
 		return 3; // 3 products per row
 	}
 }
+
+// Display product tile on top
+
+function product_change_title_position() {
+	remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_title', 5 );
+	add_action( 'woocommerce_before_single_product', 'woocommerce_template_single_title', 5 );
+}
+
+add_action( 'init', 'product_change_title_position' );
+
+// Related Products
+add_filter( 'woocommerce_output_related_products_args', 'jk_related_products_args', 20 );
+  function jk_related_products_args( $args ) {
+	$args['posts_per_page'] = 3; // 4 related products
+	$args['columns'] = 3; // arranged in 2 columns
+	return $args;
+}
+
+
